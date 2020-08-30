183767b6d0c78ca71401df7078a0bc7e58d6b5c9
Justin Chang <jchang0916@ucla.edu>
2020-08-29 19:09:48 -0700

    updated w/ rock paper scissors

--- example_bot.py
+++ example_bot.py
@@ -1,10 +1,61 @@
 import discord
 import random 
 # import json (for later)? idk 
+import discord
+from random import randint
 
 TOKEN='SOMETHING_SECRET_HAHA_IAINTJHAGINVINTOAJDSFL'
 
 client = discord.Client()
+
+
+def humanActions(human_input):
+    humanAction=human_input
+    if(humanAction=='R'):
+        
+        return "rock"
+    elif(humanAction=='P'):
+        
+        return "paper"
+    elif(humanAction=='S'):
+        
+        return "scissors"
+    else:
+        #await message.channel.send('Ok human idk what u sayin')
+        return 
+
+
+#Robot function
+def robotActions():
+    randomNum_Robot=randint(0, 2)
+    listofActions=['rock', 'paper', 'scissors']
+    return listofActions[randomNum_Robot]
+
+#who wins the game?
+def comparison(humansMove,robotsMove):
+    if(humansMove==robotsMove):
+        #await message.channel.send('TIED')
+        return "TIED"
+    elif(humansMove=='rock'):
+        if(robotsMove=='paper'):
+            #await message.channel.send('PEPE WINS')
+            return "PEPE WINS"
+        elif(robotsMove=='scissors'):
+            #await message.channel.send('YOU WIN')
+            return "YOU WIN"
+    elif(humansMove=='paper'):
+        if(robotsMove=='rock'):
+            #await message.channel.send('YOU WIN')
+            return "YOU WIN"
+        elif(robotsMove=='scissors'):
+            #await message.channel.send('PEPE WINS') 
+            return "PEPE WINS"
+    elif(humansMove=='scissors'):
+        if(robotsMove=='rock'):
+            return "PEPE WINS"
+        elif(robotsMove=='paper'):
+            return "YOU WIN"
+
 
 @client.event
 async def on_ready():
@@ -33,6 +84,37 @@ async def on_message(message):
     if message.content.startswith('pepehands'):
         await message.channel.send('pepehands lmao!')
 
+    #rock paper scissors game, still haven't figured out events.
+    if message.content.startswith('rps'):
+        await message.channel.send('lets play rock paper scissors:')
+        await message.channel.send('($R) for Rock, ($P) for Paper, ($S) for Scissors:')
+    if message.content.startswith('$R'):
+            #humanActions('R')
+        await message.channel.send('Human plays Rock')
+        humansMove=humanActions('R')
+        robotsMove=robotActions()
+        #await message.channel.send('pepesadge')
+        await message.channel.send('Pepe picks... ')
+        await message.channel.send(robotsMove)
+        winner=comparison(humansMove,robotsMove)
+        await message.channel.send(winner) 
+    if message.content.startswith('$P'):
+        await message.channel.send('Human plays Paper')
+        humansMove=humanActions('P')
+        robotsMove=robotActions()
+        await message.channel.send('Pepe picks... ')
+        await message.channel.send(robotsMove)
+        winner=comparison(humansMove,robotsMove)
+        await message.channel.send(winner) 
+    if message.content.startswith('$S'):
+        await message.channel.send('Human plays Scissors')
+        humansMove=humanActions('S')
+        robotsMove=robotActions()
+        await message.channel.send('Pepe picks... ')
+        await message.channel.send(robotsMove)
+        winner=comparison(humansMove,robotsMove)
+        await message.channel.send(winner)  
+
 @client.event
 async def on_reaction_add(reaction, user):
     # Bot aggro react
@@ -44,3 +126,6 @@ async def on_reaction_add(reaction, user):
     if reaction.emoji == '🐸':
         await reaction.message.channel.send('pepehands lmao! 🐸🤲')
 client.run(TOKEN)
+
+
+
